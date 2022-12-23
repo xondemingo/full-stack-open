13be1b28954c80bf101af630f0f29fe1f815c823
oscarlopezgonzalez <oscarnei@outlook.es>
2022-12-23 13:02:23 +0100

    completed exercise 1.5

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,26 +1,20 @@
-import Header from "./components/Header"
-import Content from "./components/Content"
-import Total from "./components/Total"
+import Course from "./components/Course"
 
 const createPart = (name, exercises) => {
   return { name, exercises }
 }
 
 const App = () => {
-  const course = "Half stack application development"
-  const parts = [
-    createPart("Fundamentals of React", 10),
-    createPart("Using props to pass data", 7),
-    createPart("State of a component", 140)
-  ]
+  const course = {
+    name: "Half stack application development",
+    parts: [
+      createPart("Fundamentals of React", 191),
+      createPart("Using props to pass data", 73),
+      createPart("State of a component", 140)
+    ]
+  }
 
-  return (
-    <div>
-      <Header title={course} />
-      <Content parts={parts} />
-      <Total parts={parts} />
-    </div>
-  )
+  return <Course course={course} />
 }
 
 export default App
