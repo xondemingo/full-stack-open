3259160f542f9b1cf8438402c4ba52da4cf71884
oscarlopezgonzalez <oscarnei@outlook.es>
2022-12-28 11:15:48 +0100

    completed exercises 1.7-1.10

--- part1/unicafe/src/components/Statistics.js
+++ part1/unicafe/src/components/Statistics.js
@@ -1,11 +1,27 @@
 import StatisticLine from "./StatisticLine"
 
 const Statistics = ({ feedback }) => {
+    const { good, neutral, bad } = feedback
+    const total = good + neutral + bad
+
+    if (total === 0) {
+        return (
+            <div>
+                No feedback has been gathered yet
+            </div>
+        )
+    }
+
+    const average = ((good - bad) / total).toFixed(2)
+    const positivePercentage = (100 * good / total).toFixed(2) + " %"
     return (
         <div>
-            <StatisticLine text="good" score={feedback.good} />
-            <StatisticLine text="neutral" score={feedback.neutral} />
-            <StatisticLine text="bad" score={feedback.bad} />
+            <StatisticLine text="good" score={good} />
+            <StatisticLine text="neutral" score={neutral} />
+            <StatisticLine text="bad" score={bad} />
+            <StatisticLine text="all" score={total} />
+            <StatisticLine text="average" score={average} />
+            <StatisticLine text="positive" score={positivePercentage} />
         </div>
     )
 }
