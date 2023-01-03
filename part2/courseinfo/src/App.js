7d21dad3035ea1c7dbd5045b99d15a2b3a4a2350
oscarlopezgonzalez <oscarnei@outlook.es>
2023-01-03 08:16:08 +0100

    completed exercises 2.4 and 2.5

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -1,21 +1,57 @@
-import Course from "./components/Course"
-
-const createPart = (id, name, exercises) => {
-  return { name, exercises }
-}
+import ListOfCourses from "./components/ListOfCourses"
 
 const App = () => {
-  const course = {
-    name: "Half stack application development",
-    parts: [
-      createPart(1, "Fundamentals of React", 191),
-      createPart(2, "Using props to pass data", 73),
-      createPart(3, "State of a component", 140),
-      createPart(4, "Fake part", 38)
-    ]
-  }
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    }, 
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
 
-  return <Course course={course} />
+  return (
+    <div>
+      <h1>Web development curriculum</h1>
+      <ListOfCourses courses={courses} />
+    </div>
+  )
 }
 
 export default App
---
+++ part2/courseinfo/src/components/ListOfCourses.js
@@ -0,0 +1,11 @@
+import Course from "./Course"
+
+const ListOfCourses = ({ courses }) => {
+    return (
+        <div>
+            {courses.map(course => <Course key={course.id} course={course} />)}
+        </div>
+    )
+}
+
+export default ListOfCourses
