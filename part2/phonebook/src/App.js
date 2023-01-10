89966a3608e1d301b9aa9dd6fa21b2b88b0e926e
oscarlopezgonzalez <oscarnei@outlook.es>
2023-01-10 17:07:25 +0100

    completed exercise 2.8

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -3,15 +3,16 @@ import NewPersonForm from "./components/NewPersonForm"
 import PersonList from "./components/PersonList"
 
 const App = () => {
-  const createPerson = (name) => {
-    return { name }
+  const createPerson = (name, number) => {
+    return { name, number }
   }
 
   const [persons, setPersons] = useState([
-    createPerson("Arto Hellas"),
-    createPerson("Óscar López González")
+    createPerson("Arto Hellas", "39-44-5323523"),
+    createPerson("Óscar López González", "652333333")
   ])
   const [newName, setNewName] = useState("")
+  const [newNumber, setNewNumber] = useState("")
 
   const handleAddNewPerson = (event) => {
     event.preventDefault()
@@ -19,17 +20,20 @@ const App = () => {
       alert(`${newName} is already added to the phonebook`)
     }
     else {
-      setPersons(persons.concat(createPerson(newName)))
+      setPersons(persons.concat(createPerson(newName, newNumber)))
       setNewName("")
+      setNewNumber("")
     }
   } 
 
   const handleTypeNewName = (event) => setNewName(event.target.value)
+  const handleTypeNewNumber = (event) => setNewNumber(event.target.value)
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <NewPersonForm handleAddNewPerson={handleAddNewPerson} newName={newName} handleTypeNewName={handleTypeNewName} />
+      <NewPersonForm handleAddNewPerson={handleAddNewPerson} newName={newName} handleTypeNewName={handleTypeNewName} 
+        newNumber={newNumber} handleTypeNewNumber={handleTypeNewNumber} />
       <h2>Numbers</h2>
       <PersonList persons={persons} />
     </div>
--- part2/phonebook/src/components/NewPersonForm.js
+++ part2/phonebook/src/components/NewPersonForm.js
@@ -1,9 +1,16 @@
-const NewPersonForm = ({ handleAddNewPerson, newName, handleTypeNewName }) => {
+const NewPersonForm = ({ handleAddNewPerson, newName, handleTypeNewName, newNumber, handleTypeNewNumber }) => {
     return (
         <div>
             <form onSubmit={handleAddNewPerson}>
-                <label htmlFor="newName">name:</label>
-                <input id="newName" style={{ marginLeft: 3 }} value={newName} onChange={handleTypeNewName} />
+                <div>
+                    <label htmlFor="newName">name:</label>
+                    <input id="newName" style={{ marginLeft: 3 }} value={newName} onChange={handleTypeNewName} />
+                </div>
+                <div>
+                    <label htmlFor="newNumber">number:</label>
+                    <input id="newNumber" style={{ marginLeft: 3, marginTop: 3, marginBottom: 3 }} 
+                        value={newNumber} onChange={handleTypeNewNumber} />
+                </div>
                 <div>
                     <button type="submit">add</button>
                 </div>
